1f687fe8554bb1b8f3144e6d75fb16984366daf9
Timothy Pressey <timothypressey@Timothys-MacBook-Pro.local>
2017-12-10 21:08:13 -0500

    game functionality added

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,19 @@ require('../styles/styles.scss');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+
+
+//initialize puzzle information
+const problems = require('../db/labyrinth.json');
+const Labyrinth = require('./labyrinth.js');
+
+let currentpuzzle = new Labyrinth(...problems[1]);
+
+
+//set up view
 import Header from './Header.jsx';
 import Nav from './Nav.jsx';
 import CurrentPuzzle from './CurrentPuzzle.jsx';
-
-const problems = require('../db/labyrinth.json');
-const Labyrinth = require('./labyrinth.js');
 
 class App extends React.Component {
   render() {
@@ -17,18 +24,22 @@ class App extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <Nav problems={problems}/>
-          <CurrentPuzzle />
+          <CurrentPuzzle/>
         </div>
       </div>
     </div>);
   }
 }
 
-const mountNode = document.getElementById('react-root')
-
-ReactDOM.render(<App/>, mountNode);
+ReactDOM.render(<App/>, document.getElementById('react-root'));
 
 let canvas = document.getElementById('canvas');
-
-let currentpuzzle = new Labyrinth(...problems[1]);
 currentpuzzle.draw(canvas);
+
+document.addEventListener("keydown", e => {
+  currentpuzzle.move(e.key);
+  currentpuzzle.draw(canvas);
+  if (currentpuzzle.win) window.setTimeout(() => {
+    alert('You Win!');
+  }, 100);
+})
